d597bd3e210fd1ee9ffcd080da72f0267f8997b7
chardon55 <charlesdong_2000@outlook.com>
2023-06-10 09:10:13 +0800

    Update loading widget

--- loadingwidget.cpp
+++ loadingwidget.cpp
@@ -1,14 +1,17 @@
 #include "loadingwidget.hpp"
 
-#include <QLabel>
-
 LoadingWidget::LoadingWidget(QWidget *parent)
     : QWidget{parent}
 {
-    QLabel *label = new QLabel("Initializing browser window...", this);
+    layout = new QVBoxLayout(this);
+
+    label = new QLabel("Loading web page...", this);
     label->setAlignment(Qt::AlignmentFlag::AlignCenter);
-    label->setFont(QFont("sans-serif", 16));
+    label->setFont(QFont("sans-serif", 12));
+
+    layout->addWidget(label);
 
     this->resize(400, 100);
     this->setWindowFlags(this->windowFlags() | Qt::FramelessWindowHint);
+    this->raise();
 }
--- loadingwidget.hpp
+++ loadingwidget.hpp
@@ -1,6 +1,9 @@
 #ifndef LOADINGWIDGET_HPP
 #define LOADINGWIDGET_HPP
 
+#include <QLabel>
+#include <QPointer>
+#include <QVBoxLayout>
 #include <QWidget>
 
 class LoadingWidget : public QWidget
@@ -8,6 +11,11 @@ class LoadingWidget : public QWidget
     Q_OBJECT
 public:
     explicit LoadingWidget(QWidget *parent = nullptr);
+
+private:
+    QPointer<QVBoxLayout> layout;
+
+    QPointer<QLabel> label;
 
 signals:
 
