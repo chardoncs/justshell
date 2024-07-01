23562721bf29f250b4c7685790836b88ca2b5793
Charles Dong <chardon_cs@proton.me>
2024-07-02 00:50:47 +0800

    Switch to webkitgtk 6.0, which supports GTK4

--- src/gui/browser_window.rs
+++ src/gui/browser_window.rs
@@ -1,12 +1,7 @@
 use gtk::{prelude::*, Application, ApplicationWindow};
 use url::Url;
-use webkit2gtk::{UserContentManager, WebContext, WebView, WebViewExt, WebViewExtManual};
 
 pub fn new_browser_window(app: &Application, url: &Url) -> ApplicationWindow {
-    let ctx = WebContext::default().unwrap_or_else(|| WebContext::new());
-
-    let webview = WebView::new_with_context_and_user_content_manager(&ctx, &UserContentManager::new());
-
     let window = ApplicationWindow::builder()
         .application(app)
         .maximized(true)
@@ -14,8 +9,5 @@ pub fn new_browser_window(app: &Application, url: &Url) -> ApplicationWindow {
         .build();
 
     window.present();
-
-    webview.load_uri(url.to_string().as_str());
-
     window
 }
